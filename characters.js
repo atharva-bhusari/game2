d426c5cb2c0569674e07ebe60874b3976e2d090c
buzzi0204 <buzzi.atharva@gmail.com>
2017-02-26 20:00:47 +0530

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,102 @@
+<!-- Script to hold Character Variables -->
+  var characters = [];
+  var babu = {
+    "id": 1,
+    "name": "Babu Bhaiyya",
+    "attack": "Aye Kutriya!",
+    "counterAttack": "Khamba",
+    "defense": "Dhotarchor",
+    "image": "./images/babu.jpg"
+  };
+
+  var aladeen = {
+      "id": 2,
+      "name": "Admiral General Aladeen",
+      "attack": "Pointy Nuclear Weapon",
+      "counterAttack": "Gudu Gudu Gudu",
+      "defense": "Body Double",
+      "image": "./images/aladeen.jpg"
+    };
+
+    var chandler = {
+        "id": 3,
+        "name": "Chandler Bing",
+        "attack": "Joey",
+        "counterAttack": "Sarcastic Comment",
+        "defense": "Monica",
+        "image": "./images/chandler.jpg"
+      };
+
+    var gormint = {
+      "id": 4,
+     "name": "Gormint Aunty",
+     "attack": "Beep Beep Beep",
+     "counterAttack": "Ye Sab Milkar hume Pagal bana rahe hai!",
+     "defense": "Bik Gayi Hai Yeh Gormint",
+     "image": "./images/gormint.jpg"
+    }
+
+    var hermione = {
+      "id": 5,
+      "name": "Hermione Granger",
+      "attack": "Slapped You Like Malfoy",
+      "counterAttack": "Time Turner",
+      "defense": "Patronus",
+      "image": "./images/hermione.jpg"
+    }
+
+    var barney = {
+      "id": 6,
+      "name": "Barney Stinson",
+      "attack": "Use The Playbook",
+      "counterAttack": "Counter - Wait For It - Attack",
+      "defense": "Armoured Suit",
+      "image": "./images/barney.png"
+    }
+
+    var joey = {
+      "id": 7,
+      "name": "Joey Tribbiani",
+      "attack": "How You Doin'",
+      "counterAttack": "Doesn't Share Food",
+      "defense": "Joey's Special - Two Pizzas",
+      "image": "./images/joey.jpg"
+    }
+
+    var roshesh = {
+      "id": 8,
+     "name": "Rosesh Sarabhai",
+     "attack": "Recite a Poem",
+     "counterAttack": "Gudu Gudu Gudu",
+     "defense": "Momma!",
+     "image": "./images/roshesh.jpg"
+    }
+
+    var sheldon = {
+      "id": 9,
+     "name": "Sheldon Cooper",
+     "attack": "Sheldon Used The Force",
+     "counterAttack": "Bazinga!",
+     "defense": "That's my Spot!",
+     "image": "./images/sheldon.jpg"
+    }
+
+    var trump = {
+      "id": 10,
+      "name": "President Trump",
+      "attack": "Ban.",
+      "counterAttack": "Fake News",
+      "defense": "Calls up Putin",
+      "image": "./images/trump.jpg"
+    }
+
+  characters.push(babu);
+  characters.push(aladeen);
+  characters.push(chandler);
+  characters.push(gormint);
+  characters.push(hermione);
+  characters.push(barney);
+  characters.push(joey);
+  characters.push(roshesh);
+  characters.push(sheldon);
+  characters.push(trump);
